5f3d5a7f47f46e5486aace1589a99a8b39bc3906
Niklas Fähnrich <niklas.fahnrich@safeture.com>
2025-09-15 11:20:58 +0200

    changes in employee on master

--- GIT/Employee.cs
+++ GIT/Employee.cs
@@ -8,5 +8,6 @@ namespace GIT;
 internal class Employee
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string Password { get; set; }
 }
