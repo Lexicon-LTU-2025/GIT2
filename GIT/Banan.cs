9d9a40dad92f6a68d0a795aee94453c22bc186bb
DZM77 <dimitris.bjorlingh@outlook.com>
2025-09-15 11:22:47 +0200

    Add Banan

---
+++ GIT/Banan.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIT;
+internal class Banan
+{
+    public string Name { get; set; }
+}
